217a91063a6c5d00d12286caeb8ea198275831bf
agustin <agustind1212@gmail.com>
2025-01-31 14:34:35 +0100

    api dog

--- 07-Apis/Dog/api_dog_aleatorio.php
+++ 07-Apis/Dog/api_dog_aleatorio.php
@@ -1,38 +1,59 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Perro aleatorio</title>
 </head>
 <body>
+
+
     <?php
-        $apiUrl = "https://dog.ceo/api/breed/affenpinscher/images/random";
+
+        $apiUrlName = "https://dog.ceo/api/breeds/list/all";
+
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $apiUrl);
+        curl_setopt($curl, CURLOPT_URL, $apiUrlName);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $respuesta = curl_exec($curl);
+        curl_close($curl);
+
+        $datos = json_decode($respuesta, true);
+        $names = $datos["message"];
+
+    ?>
+
+    <form method="get">
+        <label for="razas">Selecciona una raza:</label>
+        <select name="razas" id="razas">
+            <?php foreach ($names as $key => $value) { ?>
+                <option value="" hidden>-- Selecciona una raza --</option>
+                <option value="<?php echo $key ?>">
+                    <?php echo $key ?>
+                </option>
+            <?php } ?>
+        </select>
+    </form>
+
+    
+    <?php
+
+        $razaPerro = $_GET["razas"];
+
+        $apiUrlImg = "https://dog.ceo/api/breed//images/random";
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $apiUrlImg);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $respuesta = curl_exec($curl);
         curl_close($curl);
 
         $datos = json_decode($respuesta, true);
         $dogs = $datos["message"];
-        
 
-        ?>
+    ?>
 
-        <div class="mb-3">
-                <label class="form-label">Raza</label>
-                <select class="form-select" name="raza">
-                    <option value="" selected disabled hidden>--- Elige la raza ---</option>
-                    <?php
-                    foreach($dogs as $dog) { ?>
-                        <option value="<?php echo $dog["message"] ?>">
-                            <?php echo $dog ?>
-                        </option>
-                    <?php } ?>
-                </select>
-            </div>
-        <img src="<?php echo $dogs ?>" alt="perroImagen">
-    
+    <br><img src="<?php echo $dogs ?>" alt="Perro">
+
 </body>
 </html>
